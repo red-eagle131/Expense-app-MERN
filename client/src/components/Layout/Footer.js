80b7f58ffb74160e4b1315f1697cc639a6080c6e
Harshit Kumar Gupta <harshitkg1234@gmail.com>
2023-04-12 13:53:40 +0530

    complete code
    test1

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-4">
+      <h6 className="text-center footer">
+        2023 - All rights reserved &copy; Harshit
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
